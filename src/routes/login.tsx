056a2327199fceaf69121301cac31295cb74109e
Kim min-jae <mjkim11084@gmail.com>
2024-10-03 00:16:25 +0900

    땜빵3

--- src/routes/login.tsx
+++ src/routes/login.tsx
@@ -1,7 +1,10 @@
+import { Navigate } from "react-router-dom";
+
 export default function Login() {
     return(
         <>
             <h1>Login Page</h1>
+            <Navigate to="/create-account"/>
         </>
     )
 }
